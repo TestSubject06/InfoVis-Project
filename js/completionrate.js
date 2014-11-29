936e66dedc3f19b7601d437b1d440da09b3d9fea
hendry-s <hsugianto3@gatech.edu>
2014-11-28 16:17:32 -0800

    parsing new acceptance rate data
    parsing all the necessary info into globalInfo, the
    temp_percentage_accepted is not working. The sorting function
    sortChart() return the correct value but won't graph correctly

--- js/completionrate.js
+++ js/completionrate.js
@@ -17,7 +17,44 @@ function drawcompletionrate(Width, Height){
   		.attr("height", h)
   	  .append("svg:g")
   	  	.attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")");
-
+  	
+  	// initialize, create and populate the globalTemp array.
+  	var globalInfo = new Array();
+  	var temp = {school: "", applied: "", accepted: "", percentageAccepted:"" };
+  	var temp_applied, temp_accepted, temp_percentage_accepted;
+  	d3.csv('data/CS4460_HighSchoolAcceptanceRates_v2.csv', function(error, data){
+  		temp_applied = d3.nest()
+				.key(function(d)	{ return d["High School Description"];})
+				.sortKeys(d3.ascending)
+				.rollup(function(d){
+				 return d3.sum(d, function(g)	{return g.Applied;});
+				}).entries(data);
+				console.log(temp_applied);
+				console.log(temp_applied.length);
+		temp_accepted = d3.nest()
+				.key(function(d)	{ return d["High School Description"];})
+				.sortKeys(d3.ascending)
+				.rollup(function(d){
+				 return d3.sum(d, function(g)	{return g.Accepted;});
+				}).entries(data);
+				console.log(temp_accepted);
+				console.log(temp_accepted.length);
+		temp_percentage_accepted = d3.nest()
+				.key(function(d)	{ return d["High School Description"];})
+				.sortKeys(d3.ascending)
+				.rollup(function(d){
+				 return d3.sum(d, function(g)	{return g["Percentage Accepted"];});
+				}).entries(data);
+				console.log(temp_percentage_accepted);
+				console.log(temp_percentage_accepted.length);
+		// populate the globalInfo array
+		for(i = 0; i < temp_applied.length; i++){
+			temp = {school:temp_applied[i].key, applied: temp_applied[i].values, accepted: temp_accepted[i].values, percentageAccepted: temp_percentage_accepted[i].values};
+			globalInfo.push(temp);
+		}
+		console.log(globalInfo);
+	});
+  	
  
   d3.csv('data/QueryHighSchoolData.csv', function(error, data){
 	// sort and get the number of graduated for each school using rollup function
@@ -90,7 +127,7 @@ function drawcompletionrate(Width, Height){
  	 	})
  	 	.on("mouseout", function(d){
  	 		svg.selectAll("text").remove();
-
+ 	 	
  	 	svg.append("text")
          .text("Number of graduated and not graduated students in Georgia Institute of Technology by high schools")
          .attr("x", 100)
@@ -101,26 +138,45 @@ function drawcompletionrate(Width, Height){
          .attr("x", 100)
          .attr("y", 27);
  	 	});
- 	
- 	// var hover = d3.selectAll("rect")
- 	// 	.append("div")
- 	// 	.style("position", "absolute")
- 	// 	.style("z-index", "10")
- 	// 	.style("visibility", "visible")
- 	// 	.text("test");
+ 	 	// .on("click", function(d){
+ 	 	// 	sortChart();
+ 	 	// });	
 
- 	// Add a label per high school
- 	// var label = svg.selectAll("text")
- 	// 	.data(x.domain())
- 	//   .enter().append("text")
- 	//   	//.attr("x", function(d) {return x(d) + x.rangeBand() / 2;})
- 	//   	//.attr("y", 6)
- 	//   	.style("text-anchor", "middle")
- 	//   	//.attr("dy", ".71em")
- 	//   	.style("fill", "blue")
- 	//   	.style("transform", "( " + function(d) {return x(d) + x.rangeBand() / 2;} + ",6)")
- 	//   	.text(function(d) {return d.data;});
- 	// Add y-axis rules.
+ 	
+ 	// var sortDir = 'asc';
+ 	// var sortChart = function(){
+ 	// 	// declare array to hold re-ordered ordinal domain for xScale
+ 	// 	var newDomain = new Array();
+ 	// 	svg.selectAll('rect')
+ 	// 		.sort(function(a,b){
+ 	// 			if(sortDir == 'asc'){
+ 	// 				//console.log(a.y);
+ 	// 				//console.log(b.y);
+ 	// 				return d3.ascending(a.y, b.y);
+ 	// 			}
+ 	// 			else{
+ 	// 				return d3.descending(a.y, b.y);
+ 	// 			}
+ 	// 		})
+		// 	.transition()
+		// 	// add a delay() call before duration() call
+		// 	.delay(function(d,i){
+		// 		return i * 50;
+		// 	})
+		// 	.duration(1000)
+		// 	.attr("x", function(d, i) {return x(i);})
+ 	//   		//.attr("y", function(d) {return -y(d.y0) - y(d.y);});
+	 // 	  	//.attr("height", function(d) {return y(d.y);})
+ 	//   		//.attr("width", x.rangeBand())
+ 	//   		svg.selectAll('rect') 	  			
+ 	//   			.each(function(d){
+ 	//   				newDomain.push(highSchools);
+ 	//   		});
+ 	//   			console.log(newDomain);
+ 	//   		x.domain(newDomain[0].map(function(d) {return d.x;}));
+		// 	sortDir = sortDir = 'asc' ? 'desc' : 'asc';
+ 	// }
+
  	var rule = svg.selectAll("g.rule")
  		.data(y.ticks(5))
  	  .enter().append("svg:g")
